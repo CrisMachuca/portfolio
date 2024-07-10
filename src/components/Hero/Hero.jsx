9e3d5aaebdf6e6da35047dced0a9fbafece88a6d
Cristina Machuca <132057230+CrisMachuca@users.noreply.github.com>
2024-07-10 20:38:59 +0000

    cambio estilos hero y navbar

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,12 +7,12 @@ export const Hero = () => {
   return (
   <section className={styles.container}>
     <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I`m Cris</h1>
+        <h1 className={styles.title}>Hi, I`m Cris Machuca</h1>
         <p className={styles.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem non est temporibus corporis quaerat animi fuga quis!.</p>
         <a href="mailto:cristinamachucam@gmail.com" className={styles.contactBtn}>Contact Me</a>
     </div>
     <img 
-        src={getImageUrl("hero/heroImage.jpg")} alt="Hero image of me" className={styles.heroImg}/>
+        src={getImageUrl("hero/heroImage3.png")} alt="Hero image of me" className={styles.heroImg}/>
     <div className={styles.topBlur}></div>
     <div className={styles.bottomBlur}></div>
   </section>
