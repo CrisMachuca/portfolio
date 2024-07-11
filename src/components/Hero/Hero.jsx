d72fcc8513b64d4d36a2ff71f4f817846fd001a7
Cristina Machuca <132057230+CrisMachuca@users.noreply.github.com>
2024-07-11 21:45:57 +0000

    cambio estilos hero y foto perfil

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,12 +7,14 @@ export const Hero = () => {
   return (
   <section className={styles.container}>
     <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I`m Cris Machuca</h1>
+        <h1 className={styles.title}>I`m Cris Machuca</h1>
+        <h2 className={styles.subtitle}>Web Developer</h2>
         <p className={styles.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem non est temporibus corporis quaerat animi fuga quis!.</p>
         <a href="mailto:cristinamachucam@gmail.com" className={styles.contactBtn}>Contact Me</a>
+        <li><i className="fa-brands fa-github" /> <a href="https://github.com">Github</a></li>
     </div>
     <img 
-        src={getImageUrl("hero/heroImage3.png")} alt="Hero image of me" className={styles.heroImg}/>
+        src={getImageUrl("hero/heroImage5.png")} alt="Hero image of me" className={styles.heroImg}/>
     <div className={styles.topBlur}></div>
     <div className={styles.bottomBlur}></div>
   </section>
