48587094b0a8a6d962ed4c4722c48c0e4d71a1c8
Cristina Machuca <132057230+CrisMachuca@users.noreply.github.com>
2024-07-12 23:50:22 +0000

    about con imagen

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,12 +8,11 @@ export const About = () => {
     <section className={styles.container} id="about">
         
         <h2 className={styles.title}>About Me.</h2>
-        
-        
-        <p className={styles.subtitle}>Hi, my name is Cristina Machuca and I've spent most of my waking hours for the last  years learning, designing, programming and operating Web sites. Apart from this I love to travel, the rock music and everything related to web development. I am deeply committed to the success of every project I work on and take great joy in the development process, from initial concept to final deployment. I believe in the power of collaboration and creativity to build exceptional websites and applications.</p>
+        <div className={styles.aboutItems}>
+        <img className={styles.aboutImage} src={getImageUrl("about/33551.jpg")} alt="Server icon" />
+        <p className={styles.subtitle}>Hi, my name is Cristina Machuca and I've spent most of my waking hours for the last  years learning, designing, programming and operating Web sites. Apart from this I love to travel, the rock music and everything related to web development. I am deeply committed to the success of every project I work on and take great joy in the development process, from initial concept to final deployment. I believe in the power of collaboration and creativity to build exceptional websites and applications. Let's work together to bring your ideas to life with dedication and expertise.</p>
        
-
-        <p className={styles.subtitle}>Let's work together to bring your ideas to life with dedication and expertise.</p>
+        </div>
     </section>
   );
 };
