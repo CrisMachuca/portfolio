dae91145769a433ea84fa013d2dcc7c0a01ef9a1
Cristina Machuca <132057230+CrisMachuca@users.noreply.github.com>
2024-07-14 23:26:05 +0000

    colores filtros botones

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,10 +6,12 @@ import { getImageUrl } from '../../utils';
 export const About = () => {
   return (
     <section className={styles.container} id="about">
+      <h2 className={styles.title}>About Me.</h2>
         <div className={styles.aboutItems}>
+        
           <img className={styles.aboutImage} src={getImageUrl("about/about.png")} alt="Server icon" />
           <div className={styles.aboutText}>
-            <h2 className={styles.title}>About Me.</h2>
+            
             <p className={styles.subtitle}>Hi, my name is <span className={styles.accent}>Cristina Machuca</span>  and I've spent most of my waking hours for the last  years learning, designing, programming and operating Web sites. Apart from this I love to travel, the rock music and everything related to web development. I am deeply committed to the success of every project I work on and take great joy in the development process, from initial concept to final deployment.</p>
             
             <button className={styles.connect}>Let's Connect</button>
