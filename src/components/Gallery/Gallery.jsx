3eb498b37c6a35c94ba9922e2c40fd90869845ba
Cristina Machuca <132057230+CrisMachuca@users.noreply.github.com>
2024-07-16 22:33:54 +0000

    elimino imagenes sin uso y agrego figcaption a gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { FaHeart, FaFacebook, FaGithub, FaLinkedin, FaDownload, FaEye, FaPaperclip } from "react-icons/fa";
+
 import styles from "./Gallery.module.css"
 import { getImageUrl } from '../../utils';
 
@@ -7,18 +9,27 @@ export const Gallery = ({ project }) => {
     const { title, imageSrc, description, skills } = project;
   
     return (
+      
       <div className={styles.gallery}>
-        <div className={styles.galleryItem}> 
-          <img 
-            src={getImageUrl(imageSrc)} 
-            alt={`Image of ${title}`} 
-            className={styles.image}
-          />
-          <h2 className={styles.title}>{title}</h2>
-          <span className={styles.skills}>{skills}</span>
-          <div className={styles.caption}>{description}</div>
-        </div>
-      </div>
+      <figure className={styles.effectZoe}>
+        <img 
+          src={getImageUrl(imageSrc)} 
+          alt={`Image of ${title}`} 
+          className={styles.image}
+        />
+        <figcaption>
+          <h2>{title}</h2>
+          <p className={styles.iconLinks}>
+            <a href="#"><FaHeart className={styles.iconHeart}/></a>
+            <a href="#"><FaEye className={styles.iconEye}/></a>
+            <a href="#"><FaPaperclip className={styles.iconPaper}/></a>
+          </p>
+          <p className={styles.description}>{description}</p>
+        </figcaption>			
+      </figure>
+    </div>
+      
+      
     );
   };
 
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -31,7 +31,7 @@ export const Hero = () => {
         </div>
           <p className={styles.description}>From front-end aesthetics to back-end logic, I craft complete digital solutions. Ready to innovate together?</p>
           <div className={styles.heroBtns}>
-            <a href="/CristinaMachucaResume.pdf" download className={styles.contactBtn}>Download CV <FaDownload /></a>
+            <a href="/CristinaMachucaResume.pdf" download className={styles.contactBtn}>Download CV <FaDownload /></a> 
             <a href=""><FaLinkedin className={styles.social}/></a>
             <a href=""><FaGithub className={styles.social}/></a>
           </div>
