23d9e96ea2a7e5d2fa336bde53c2bc8a572833ea
Cristina Machuca <132057230+CrisMachuca@users.noreply.github.com>
2024-07-19 22:26:51 +0000

    mejoro gallery, imagenes mas grandes 2 columnas - 1

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,35 +1,24 @@
-import React from 'react'
-
-import { FaHeart, FaFacebook, FaGithub, FaLinkedin, FaDownload, FaEye, FaPaperclip } from "react-icons/fa";
-
-import styles from "./Gallery.module.css"
+import React, { useState } from 'react';
+import styles from "./Gallery.module.css";
 import { getImageUrl } from '../../utils';
 
 export const Gallery = ({ project }) => {
-    const { title, imageSrc, description, skills } = project;
-  
-    return (
-      
-      <div className={styles.gallery}>
-      <figure className={styles.effectZoe}>
-        <img 
-          src={getImageUrl(imageSrc)} 
-          alt={`Image of ${title}`} 
-          className={styles.image}
-        />
-        <figcaption>
-          <h2>{title}</h2>
-          <p className={styles.iconLinks}>
-            <a href="#"><FaHeart className={styles.iconHeart}/></a>
-            <a href="#"><FaEye className={styles.iconEye}/></a>
-            <a href="#"><FaPaperclip className={styles.iconPaper}/></a>
-          </p>
-          <p className={styles.description}>{description}</p>
-        </figcaption>			
-      </figure>
-    </div>
-      
-      
-    );
-  };
+    const { title, imageSrc, description } = project;
+    
 
+    return (
+        <div className={styles.gallery}>
+            <div className={styles.effectZoe}>
+                <img 
+                    src={getImageUrl(imageSrc)} 
+                    alt={`Image of ${title}`} 
+                    className={styles.image}
+                />
+            </div>
+            <div className={styles.underImage}>
+                <h2>{title}</h2>
+                <p className={styles.description}>{description}</p>
+            </div>
+        </div>
+    );
+};
